3baff71573718a0a71c2c7820a2590cd419aac7c
amyrabdul <amyrabdul@hotmail.com>
2020-09-11 20:24:53 -0300

    exercicio cidades

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Essas sao as cidades do Litoral sul!");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Cubatão");
+            Console.WriteLine("Caraguatatuba");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Ubatuba");
+            Console.WriteLine("São Sebastião");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("IlhaBela");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Ilha Comprida");
+            Console.ReadKey ();
+        }
+    }
+}
